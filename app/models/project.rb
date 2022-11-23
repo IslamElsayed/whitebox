5605d71224ed5e2df3545d5bca5149b1740f7eef
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2022-11-23 17:07:27 +0200

    add rubocop

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  belongs_to :owner
+  has_many :collaborators, class_name: 'User'
 end
--- spec/views/projects/edit.html.erb_spec.rb
+++ spec/views/projects/edit.html.erb_spec.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "projects/edit", type: :view do
-  let(:project) {
+RSpec.describe 'projects/edit', type: :view do
+  let(:project) do
     Project.create!(
-      name: "MyString"
+      name: 'MyString'
     )
-  }
+  end
 
   before(:each) do
     assign(:project, project)
   end
 
-  it "renders the edit project form" do
+  it 'renders the edit project form' do
     render
 
-    assert_select "form[action=?][method=?]", project_path(project), "post" do
-
-      assert_select "input[name=?]", "project[name]"
+    assert_select 'form[action=?][method=?]', project_path(project), 'post' do
+      assert_select 'input[name=?]', 'project[name]'
     end
   end
 end
