9217f902825a4cf084057ea8ff9864ab3a0966b9
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2022-11-27 22:26:47 +0200

    delegate attributes

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,12 @@
 
 class Task < ApplicationRecord
   belongs_to :project
-  belongs_to :creator, class_name: 'User'
-  belongs_to :assignee, class_name: 'User'
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id', optional: true
 
   enum status: %i[to_do in_progress done]
+
+  delegate :name, to: :creator, prefix: true
+  delegate :name, to: :assignee, prefix: true
+  delegate :name, to: :project, prefix: true
 end
