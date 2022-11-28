d58b248013e107e750aeb6badba8df2a4dc70f7c
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2022-11-28 13:36:53 +0200

    add description to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,50 +12,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_195605) do
-  create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_221_128_112_353) do
+  create_table 'projects', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "projects_users", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "project_id", null: false
-    t.integer "role", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_projects_users_on_project_id"
-    t.index ["user_id"], name: "index_projects_users_on_user_id"
+  create_table 'projects_users', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'project_id', null: false
+    t.integer 'role', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['project_id'], name: 'index_projects_users_on_project_id'
+    t.index ['user_id'], name: 'index_projects_users_on_user_id'
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.integer "status", default: 0
-    t.integer "project_id", null: false
-    t.integer "creator_id"
-    t.integer "assignee_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["assignee_id"], name: "index_tasks_on_assignee_id"
-    t.index ["creator_id"], name: "index_tasks_on_creator_id"
-    t.index ["project_id"], name: "index_tasks_on_project_id"
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'status', default: 0
+    t.integer 'project_id', null: false
+    t.integer 'creator_id'
+    t.integer 'assignee_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.text 'description'
+    t.index ['assignee_id'], name: 'index_tasks_on_assignee_id'
+    t.index ['creator_id'], name: 'index_tasks_on_creator_id'
+    t.index ['project_id'], name: 'index_tasks_on_project_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'name'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "projects_users", "projects"
-  add_foreign_key "projects_users", "users"
-  add_foreign_key "tasks", "projects"
+  add_foreign_key 'projects_users', 'projects'
+  add_foreign_key 'projects_users', 'users'
+  add_foreign_key 'tasks', 'projects'
 end
